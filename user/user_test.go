f28c1a45060710ad58299a09f064cf8e7c18d4c6
Joonkyo Kim <jkkim@ino-on.com>
2021-05-27 11:18:58 +0900

    MGIDEV-1346: Fix JWT parsing bug

--- user/user_test.go
+++ user/user_test.go
@@ -4,8 +4,12 @@ import (
 	"fmt"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/oauth2"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func init() {
@@ -49,4 +53,29 @@ func TestUserGetDeviceTokenNonExistUser(t *testing.T) {
 
 	assert.Nil(t, err)
 	assert.True(t, len(tokens) == 0)
+}
+
+//
+// Test Auth token
+//
+func TestInvalidToken(t *testing.T) {
+	dummyTokens := []string{
+		"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+		"",
+		"hello",
+	}
+
+	for _, dummyToken := range dummyTokens {
+		oauthToken := &oauth2.Token{
+			AccessToken: dummyToken,
+			TokenType:   "bearer",
+			Expiry:      time.Now().Add(24 * time.Hour),
+		}
+		cli := &client{oauthToken: oauthToken}
+
+		_, err := cli.GetDeviceToken("jkkim@ino-on.com")
+
+		assert.NotEqual(t, codes.Unavailable, status.Code(err))
+		assert.NotNil(t, err)
+	}
 }
